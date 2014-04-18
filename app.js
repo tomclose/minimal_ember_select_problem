4d9f423cc05deb4ae2029169054f395b2c6c2817
Tom Close <tom.close@cantab.net>
2014-04-18 16:25:09 +0100

    Unable to recreate problem at the moment ...

---
+++ app.js
@@ -0,0 +1,47 @@
+App = Ember.Application.create({});
+
+App.Router.map(function() {
+  this.resource('posts', { path: '/'} );
+});
+
+App.PostsRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('post');
+  },
+  setupController: function(controller, model) {
+    controller.set('model', model);
+    this.controllerFor('authors').set('model', this.store.find('author'));
+  }
+});
+
+App.Post = DS.Model.extend({
+  title:  DS.attr('string'),
+  author: DS.belongsTo('author'),
+});
+
+App.Author = DS.Model.extend({
+  name: DS.attr('string'),
+});
+
+App.AuthorsController = Ember.ArrayController.extend();
+
+App.PostController = Ember.ObjectController.extend({
+  needs: ['authors'],
+  authors: Ember.computed.alias('controllers.authors'),
+
+  names: ["Tom", "Bob"]
+});
+
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+var posts = [
+  { id: '1', title: 'First post', author: '1'},
+  { id: '2', title: 'Second post', author: '2'}
+];
+App.Post.FIXTURES = posts;
+
+var authors = [
+  {id: '1', name: 'Alice'},
+  {id: '2', name: 'Bob'},
+];
+App.Author.FIXTURES = authors;
